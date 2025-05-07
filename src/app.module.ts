8a72a84a55eb2809f92f41d82f84c485f47729b1
Celestine <ejioforcelestine77@gmail.com>
2025-05-07 07:27:01 +0100

    add oracle metadata

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OracleModule } from './oracle/oracle.module';
 
 @Module({
-  imports: [],
+  imports: [OracleModule],
   controllers: [AppController],
   providers: [AppService],
 })
